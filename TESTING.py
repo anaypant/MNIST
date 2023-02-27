e212f75e15ddf0bf5f6c712f17aa1824e835ae7f
Anay Pant <anaypant212@gmail.com>
2023-02-26 17:12:53 -0800

    final

--- TESTING.py
+++ TESTING.py
@@ -13,19 +13,28 @@ data_name = input("What is the name of your dataset (Excluding .csv): ")
 
 # This is more interesting
 pygame.init()
-df = pd.read_csv("datasets/"+data_name+".csv")["label"].unique().tolist()
-if data_name== "MNIST":df = sorted(df)
-print(df)
-with open("weights/W1_" + str(data_name)+".bin", "rb") as f:
-    W1 = pickle.load(f)
-with open("weights/W2_" + str(data_name)+".bin", "rb") as g:
-    W2 = pickle.load(g)
+
+if data_name == "MNIST":
+    with open("weights/W1_" + str(data_name) + ".bin", "rb") as f:
+        W1 = pickle.load(f)
+    with open("weights/W2_" + str(data_name) + ".bin", "rb") as g:
+        W2 = pickle.load(g)
+else:
+    df = pd.read_csv("datasets/" + data_name + ".csv")["label"].unique().tolist()
+    if data_name == "MNIST":
+        df = sorted(df)
+    print(df)
+    with open("weights/W1_" + str(data_name) + ".bin", "rb") as f:
+        W1 = pickle.load(f)
+    with open("weights/W2_" + str(data_name) + ".bin", "rb") as g:
+        W2 = pickle.load(g)
 
 
 def tanh(x, deriv=False):
     if deriv:
         return 1.0 - (np.power(x, 2))
     return np.tanh(x)
+
 
 def softmax(x):
     e_x = np.exp(x - np.max(x))
@@ -48,30 +57,34 @@ while True:
             sys.exit()
     if win32api.GetKeyState(0x01) < 0:
         mX, mY = pygame.mouse.get_pos()
-        i = mY//CELL_W
-        j = mX//CELL_W
+        i = mY // CELL_W
+        j = mX // CELL_W
         grid[i][j] += random.uniform(0.2, 0.6)
         grid[i][j] = min(1.0, grid[i][j])
         if i >= 1:
-            grid[i-1][j] += random.uniform(0.05, 0.2)
-            grid[i-1][j] = min(1.0, grid[i-1][j])
-        if (i < 27):
-            grid[i+1][j] += random.uniform(0.05, 0.2)
-            grid[i+1][j] = min(1.0, grid[i+1][j])
+            grid[i - 1][j] += random.uniform(0.05, 0.2)
+            grid[i - 1][j] = min(1.0, grid[i - 1][j])
+        if i < 27:
+            grid[i + 1][j] += random.uniform(0.05, 0.2)
+            grid[i + 1][j] = min(1.0, grid[i + 1][j])
         if j >= 1:
-            grid[i][j-1] += random.uniform(0.05, 0.2)
-            grid[i][j-1] = min(1.0, grid[i][j-1])
-        if (j < 27):
-            grid[i][j+1] += random.uniform(0.05, 0.2)
-            grid[i][j+1] = min(1.0, grid[i][j+1])
+            grid[i][j - 1] += random.uniform(0.05, 0.2)
+            grid[i][j - 1] = min(1.0, grid[i][j - 1])
+        if j < 27:
+            grid[i][j + 1] += random.uniform(0.05, 0.2)
+            grid[i][j + 1] = min(1.0, grid[i][j + 1])
 
         newGrid = np.array(grid, copy=True).flatten()
     z1 = newGrid.dot(W1)
     a1 = tanh(z1)
     z2 = a1.dot(W2)
     a2 = softmax(z2)
-    pygame.display.set_caption("I predict:  " + str(df[np.argmax(a2)]))
-    if (keyboard.is_pressed("c")):
+    if data_name == "MNIST":
+        ret = str(np.argmax(a2))
+    else:
+        ret = str(df[np.argmax(a2)])
+    pygame.display.set_caption("I predict:  " + ret)
+    if keyboard.is_pressed("c"):
         grid = []
         for i in range(28):
             grid.append([])
@@ -80,8 +93,10 @@ while True:
 
     for i in range(28):
         for j in range(28):
-
-            pygame.draw.rect(screen, (grid[i][j] * 255.0, grid[i][j] * 255.0,
-                             grid[i][j] * 255.0), [j*CELL_W, i*CELL_W, CELL_W, CELL_W])
+            pygame.draw.rect(
+                screen,
+                (grid[i][j] * 255.0, grid[i][j] * 255.0, grid[i][j] * 255.0),
+                [j * CELL_W, i * CELL_W, CELL_W, CELL_W],
+            )
     pygame.display.update()
     clock.tick(60)
